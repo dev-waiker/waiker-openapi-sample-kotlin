bf74da4c9f29c1e32b7ade224181b2f0a2290aaa
dev-waiker <85266830+dev-waiker@users.noreply.github.com>
2024-06-18 13:40:02 +0900

    Create sample-websocket.kt

---
+++ sample-websocket.kt
@@ -0,0 +1,24 @@
+class ClientWebSocketStompConfig {
+    fun webSocketStompClient(
+        websocketStompClient: WebSocketStompClient,
+        stompSessionHandler: StompSessionHandler
+    ): WebSocketStompClient {
+        websocketStompClient.messageConverter = MappingJackson2MessageConverter()
+
+        val headers = StompHeaders()
+        val url = "wss://oapi.waiker.ai/v2/center-ws"
+        websocketStompClient.connect(url, null, headers, stompSessionHandler)
+
+        return websocketStompClient
+    }
+}
+
+class ClientWebSocketStompSessionHandler : StompSessionHandlerAdapter() {
+    override fun afterConnected(session: StompSession, connectedHeaders: StompHeaders) {
+        val headers = StompHeaders()
+        headers.add("Waiker-Product-Key", "발급다은 웨이커 프로덕트 키");
+        headers.add("Authorization", "Bearer ${jwtToken}");
+        headers.add("destination", "/news/{regionCode}")
+        session.subscribe(headers, this)
+    }
+}
