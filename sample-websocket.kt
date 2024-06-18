42a2bc38767bada06ab2b2f706dc68d31b587d23
dev-waiker <85266830+dev-waiker@users.noreply.github.com>
2024-06-18 13:49:02 +0900

    Update sample-websocket.kt

--- sample-websocket.kt
+++ sample-websocket.kt
@@ -16,7 +16,7 @@ class ClientWebSocketStompConfig {
 class ClientWebSocketStompSessionHandler : StompSessionHandlerAdapter() {
     override fun afterConnected(session: StompSession, connectedHeaders: StompHeaders) {
         val headers = StompHeaders()
-        headers.add("Waiker-Product-Key", "발급다은 웨이커 프로덕트 키");
+        headers.add("Waiker-Product-Key", "발급받은 웨이커 프로덕트 키");
         headers.add("Authorization", "Bearer ${jwtToken}");
         headers.add("destination", "/news/{regionCode}")
         session.subscribe(headers, this)
